ec614fa0dd33148adbb15767e8ccb3ac2180d4fb
Asma-malica <115386995+Asma-malica@users.noreply.github.com>
2024-06-16 08:53:08 +0530

    initial

---
+++ D2-Components/src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+// import Hello from './App.jsx'
+import {Hello,Hey} from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Hello />
+    <Hey />
+  </React.StrictMode>,
+)
+
+
+
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//  <h1>asdfghjk</h1>
+// )
